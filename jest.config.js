03c4e30974fe888cccbc35ecfa38ef6cb26302ea
ericywl <ericywl@live.com>
2019-10-25 23:31:02 +0800

    Move test files back to common

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
     "^Data(.*)$": "<rootDir>/data/config",
     "^Components(.*)$": "<rootDir>/src/components$1",
-    "^Common(.*)$": "<rootDir>/src/components/common$1",
+    "^Common(.*)$": "<rootDir>/src/common$1",
     "^Static(.*)$": "<rootDir>/static$1",
   },
   testPathIgnorePatterns: [`node_modules`, `.cache`, `public`],
