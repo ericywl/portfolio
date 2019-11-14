0c6f0c9eb057a36da74c3aaa7be1c8fa00e99493
ericywl <ericywl@live.com>
2019-11-14 21:35:57 +0800

    Update test to reflect changes to Header

--- src/common/Header/test.js
+++ src/common/Header/test.js
@@ -1,8 +1,8 @@
 import React from "react"
 import renderer from "react-test-renderer"
+import AnchorLink from "react-anchor-link-smooth-scroll"
 
 import Header from "./index"
-import { SideLinks } from "./styles"
 
 describe("Header", () => {
   it("renders correctly", () => {
@@ -12,11 +12,11 @@ describe("Header", () => {
   
   it("renders AnchorLinks when in normal pages", () => {
     const component = renderer.create(<Header noAnchor={false} />).root
-    expect(component.findAllByType(SideLinks).length).toBe(1)
+    expect(component.findAllByType(AnchorLink).length).toBe(2)
   })
   
   it("does not render AnchorLinks when in NotFound or Blog page", () => {
     const component = renderer.create(<Header noAnchor={true} />).root
-    expect(component.findAllByType(SideLinks).length).toBe(0)
+    expect(component.findAllByType(AnchorLink).length).toBe(0)
   })
 })
